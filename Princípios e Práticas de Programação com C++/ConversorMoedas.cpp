7a8d845703a4b63ce087fc5e4287308dc7a66569
MahSomething <lordismail.iii@gmail.com>
2022-09-01 21:02:33 +0200

    Conversor de Moedas (Sem o uso de Switch)

---
+++ Princípios e Práticas de Programação com C++/ConversorMoedas.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+int main() {
+  double dollar = 0.00;
+  double moeda = 0.00;
+  char unidade = ' ';
+std::string nome = "";
+  std::cout<<"Ola! Seja Benvindo ao seu Conversor de Moedas extrangeiras para o dollar."<<'\n'<<'\n';
+  std::cout<<"Para podermos continuar porfavor poderia Informar o seu nome?"<<'\n'<<'\n';
+  std::cin>>nome;
+  if(nome=="nao")
+  {
+    std::cout<<"\nOpah! Sentimos muito, mas nao podemos trabalhar com quem nao pode nos confiar com o seu nome"<<'\n';
+  }
+  else {
+  std::cout<<"Ola "<<nome<<" Obrigado por fornecer o seu nome, nos agraecemos"<<'\n'<<'\n';
+  std::cout<<"Por favor informe a quantidade de moedas que pretende converter seguido da unidade da moeda"<<'\n'<<'\n';
+  std::cin>>moeda;
+  std::cin>>unidade;
+  if(unidade=='i')
+  {
+    dollar=moeda*2;
+    std::cout<<"O valor em dollar da moeda "<<unidade<<" e de $"<<dollar<<'\n';
+  }
+  else if (unidade=='e')
+  {
+    dollar=moeda*3;
+    std::cout<<"O valor em dollar da moeda "<<unidade<<" e de $"<<dollar<<'\n';
+  }
+    else if (unidade=='l')
+  {
+    dollar=moeda*4;
+    std::cout<<"O valor em dollar da moeda "<<unidade<<" e de $"<<dollar<<'\n';
+  }
+  /*while(unidade==' ')
+  {
+    std::cout<<"A unidade nao deve ser fornecidade"<<'\n';
+    std::cin>>unidade;
+  }*/
+  std::cout<<"Desculpa, mas nao conhecemos tal unidade de moeda"<<'\n';
+}
+  return 0;
+}
